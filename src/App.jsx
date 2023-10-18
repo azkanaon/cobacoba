8f1943ca68469900fa0d47e132ec5d59a8d6668d
32602100045_Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2023-10-18 19:40:02 +0700

    feature: addPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Fatwa from "./pages/Fatwa";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/fatwa" element={<Fatwa />}></Route>
         </Routes>
       </Router>
     </>
