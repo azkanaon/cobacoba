94b4a1d15c1c6ba8640270d7218bcafa576dc046
Kevin A. Marpaung <98822231+KevinMarpaung@users.noreply.github.com>
2023-10-18 19:51:36 +0700

    page kevin (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Kevin from "./pages/Kevin";
 import Fatwa from "./pages/Fatwa";
 import Azka from "./pages/Azka";
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/kevin" element={<Kevin />}></Route>
           <Route path="/fatwa" element={<Fatwa />}></Route>
           <Route path="/azka" element={<Azka />}></Route>
         </Routes>
